7614a2a2008a9c09a24c29018c43337acb67b2d0
CalebNdumbu <calebkyamaweb@gmail.com>
2024-10-30 10:14:31 +0300

    Complete SumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+  if (!Number.isInteger(int1) || !Number.isInteger(int2)) {
+    return "ERROR";
+  }
 
+  if (int1 < 0 || int2 < 0) {
+    return "ERROR";
+  }
+  let sum = 0;
+
+  const start = Math.min(int1, int2);
+  const end = Math.max(int1, int2);
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
