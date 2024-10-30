a84da00c2bbdb4a3985be3c1c65035a2f52a839f
CalebNdumbu <calebkyamaweb@gmail.com>
2024-10-30 09:46:19 +0300

    Complte removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...nums) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (nums.includes(arr[i])) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
