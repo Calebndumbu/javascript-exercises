999d1649c6570922358302cec301af8aae3c79d2
CalebNdumbu <calebkyamaweb@gmail.com>
2024-10-31 13:59:17 +0300

    temp Conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,24 @@
-const convertToCelsius = function() {
-};
+function convertToCelsius(fahrenheit) {
+  const celsius = ((fahrenheit - 32) * 5) / 9;
+  const roundedCelsius = parseFloat(celsius.toFixed(1));
+  return Number.isInteger(roundedCelsius) ? roundedCelsius | 0 : roundedCelsius;
+}
 
-const convertToFahrenheit = function() {
-};
+function convertToFahrenheit(celsius) {
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  const roundedFahrenheit = parseFloat(fahrenheit.toFixed(1));
+  return Number.isInteger(roundedFahrenheit)
+    ? roundedFahrenheit | 0
+    : roundedFahrenheit;
+}
+//toFixed(1): Rounds to one decimal place and
+//returns a string.
+
+//parseFloat(): Converts the result back to a number
+//after rounding.
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
