98eea17ca0e7e85186c5c9dbe76b20dcdb7ff3a3
CalebNdumbu <calebkyamaweb@gmail.com>
2024-10-31 16:30:31 +0300

    Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (typeof n === "string") {
+    n = Number(n);
+  }
+  if (n === 0) return 0;
+  if (n < 0) return "OOPS";
+  if (n === 1 || n === 2) return 1;
 
+  let a = 1;
+  let b = 1;
+  let fib;
+
+  for (let i = 3; i <= n; i++) {
+    fib = a + b;
+    a = b;
+    b = fib;
+  }
+  return fib;
 };
 
 // Do not edit below this line
