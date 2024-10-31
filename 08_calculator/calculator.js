42b27e3ca70ae8ce19b3450a5d1b2d04e1e6170b
CalebNdumbu <calebkyamaweb@gmail.com>
2024-10-31 14:46:25 +0300

    Finish calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let total = 0;
+  for (const num of arr) {
+    total += num;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let product = 1;
+  for (const num of arr) {
+    product *= num;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  total = 1;
+
+  if (num == 0) {
+    return 1;
+  }
+
+  for (let i = num; i > 1; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
