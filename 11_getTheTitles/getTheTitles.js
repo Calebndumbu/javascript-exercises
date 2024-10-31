22e9a9120af30e6894836ea7f53bc62811d1c5e1
CalebNdumbu <calebkyamaweb@gmail.com>
2024-10-31 16:51:58 +0300

    Get Titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (arr) {
+  let titles = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    let currentTitle = arr[i].title;
+    titles.push(currentTitle);
+  }
+  return titles;
 };
 
 // Do not edit below this line
