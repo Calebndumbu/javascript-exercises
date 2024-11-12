849de31cd41b819ab20158ebfcfb1aae053a1d12
CalebNdumbu <calebkyamaweb@gmail.com>
2024-11-12 07:12:21 +0300

    Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
+// const getAge = function (birth, death) {
+//   if (!death) {
+//     death = new Date().getFullYear();
+//   }
+//   return death - birth;
+// };
 
+// const findTheOldest = function (people) {
+//   return people.reduce((oldest, currentPerson) => {
+//     const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+//     const currentAge = getAge(
+//       currentPerson.yearOfBirth,
+//       currentPerson.yearOfDeath
+//     );
+//     return oldestAge < currentAge ? currentPerson : oldest;
+//   });
+// };
+
+const getAge = function (person) {
+  person.yearOfDeath ??= new Date().getFullYear();
+  return person.yearOfDeath - person.yearOfBirth;
+};
+
+const findTheOldest = function (people) {
+  const peopleOldestToYoungest = people.toSorted(
+    (person, nextPerson) => getAge(nextPerson) - getAge(person)
+  );
+
+  const oldestPerson = peopleOldestToYoungest[0];
+  return oldestPerson
 };
 
 // Do not edit below this line
